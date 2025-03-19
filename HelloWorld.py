d9620e5b3d2e489b74e3ce4f5d95aa322b934925
Kim Taemin <kimtaemin0203@gmail.com>
2025-03-19 15:14:40 +0900

    커서 사용해봄

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,10 @@
-print("Hello World")
-print("test")
+print("안녕하세요 세상!")
+print("테스트입니다")
+
+
+x = int(input("첫번째 숫자를 입력하세요: "))
+y = int(input("두번째 숫자를 입력하세요: "))
+print("덧셈:", x + y)
+print("뺄셈:", x - y) 
+print("곱셈:", x * y)
+print("나눗셈:", x / y)
